fe99c1223074c134d86a12ff423a6b78cafd0d54
Konstantin Zarudaev <kzar@microsoft.com>
2018-10-28 22:59:11 +0300

    Update function adding lines to the DB

--- test.py
+++ test.py
@@ -8,8 +8,23 @@ import csv
 import io
 import requests 
 
-token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDA3NTY5MjUsImlkIjoiYWxpc2EiLCJvcmlnX2lhdCI6MTU0MDY3MDUyNSwidXNlcmlkIjo1MTM1MDcsInVzZXJuYW1lIjoidmxrb290bW5pIn0.uNwbwGata1RgRbj_4uDJ4TgsPwfCb1ladYK0GYmYrr7hXWo50HYPxKnSqfGaspiNZEDtyXiAUH6CAmrFvNpfOFwgX1KfEhL5lfT-MHRJY7snRyZh9GOkY9LmvEy669a6xrhmMtAUfYx9NVC809ihAZAQ3xlYNlZ-xsayPvrgxA9FBth2-32QJY2fxh8SdF9RHBhRSr_w9SE993PjNpsVdu9dkKZZwAhHxcbsv3wTBBa7k70sqwyQkT4QMr9zF8MvkLBouBdVwzkOx9CtBJ_icTxIJHAZ8hsrNTVjztkDrDL2ybUORaEsKoZzcE9Jmc_TUfXb45gsh8mwYqaGjFG1Ww'
+token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDA4NDIwNTIsImlkIjoiYWxpc2EiLCJvcmlnX2lhdCI6MTU0MDc1NTY1MiwidXNlcmlkIjo1MTM1MDcsInVzZXJuYW1lIjoidmxrb290bW5pIn0.IsZxHlgf1HxaBiizZ5M90nQtId5Cv6B1iiwkUIH-ivHdBx78_RJbzawHU_jYEVadtakZc0f6kiOVKK_D1TfxiQD82rNHCcyC4-kUUTbS8JrqQxU0GgUXS8Va0HAOA9J9kxsJKHu7iCYLYotQaWBQpkq5Z__dhScnfHtLBL9_GS9XYaixNZ3SVcq6T83lVcYMJU9-eNZ7YHkPf7Aji52S5rqAEedC4FRn1YkchbPdMlPaLaFRb8MKH56_wSINuQ5b5rJTJa6hevIQZpV8MJ2PtbSEyeiSkUjG3pQy78csy7nO3UiSOOmJHTh4CcQa_OSRQbx1Fes63vE6YIK3Yaxwmw'
 
+#update tokenыукпун1989
+def tockenRefresh():
+    URL = "https://api.thetvdb.com/login"   
+    HEADERS = {'Content-Type': 'application/json'}  
+    DATA = {"apikey": "0AHRVCC9FPSYWACV", "userkey": "QOCM9N37ADVTQ42W", "username": "vlkootmni"}
+    r = requests.post(url = URL, json = DATA, headers = HEADERS)
+    data = r.json()
+    global token
+    if 'Error' in data:
+        print('Error in token request')
+        token = '0'
+    if 'token' in data:
+        print('the token has been got sussessfully')
+        token = data['token']
+ 
 def tvdbGetSerialInfo(filmID):
     URL = "https://api.thetvdb.com"    
     #filmID = '80379'
@@ -19,54 +34,101 @@ def tvdbGetSerialInfo(filmID):
     #PARAMS = {'airedSeason':seasonNumber} 
     #Create full request
     #URL = URL + '/series/' + filmID + '/episodes/query'
-    URL = URL + '/series/' + filmID
-    r = requests.get(url = URL, headers = HEADERS)  
+    URL1 = URL + '/series/' + str(filmID)
+    r = requests.get(url = URL1, headers = HEADERS)  
     # extracting data in json format 
     data = r.json()
     if 'Error' in data:
         #there is an error
-        if data['Error'] == 'Not authorized':
-            return "Auth error"
         if 'No results for your query' in data['Error']:
-            return "Search error"
-        return "Unspecified error"
-    
-    URL = URL + '/episodes/summary'
-    r2 = requests.get(url = URL, headers = HEADERS)
+            return 'Error', 'Search error'
+        if data['Error'] == 'Not authorized':
+            #try update token
+            tockenRefresh()
+            if token == '0':
+                return 'Error', 'Auth error and update token error'
+            HEADERS = {'Content-Type': 'application/json','Authorization':('Bearer ' + token)}
+            r = requests.get(url = URL1, headers = HEADERS)
+            data = r.json()
+            if 'Error' in data:
+                return 'Error', 'Error token update'
+    #collect Rus Name
+    HEADERS = {'Content-Type': 'application/json','Authorization':('Bearer ' + token),'Accept-Language':'ru'}
+    r = requests.get(url = URL1, headers = HEADERS)
+    dataru = r.json()
+    #collect info about episodes summary
+    URL2 = URL + '/series/' + str(filmID) + '/episodes/summary'
+    r2 = requests.get(url = URL2, headers = HEADERS)
     data2 = r2.json()
 
-    if 'Error' in data:
-        #there is an error
-        if data2['Error'] == 'Not authorized':
-            return "Auth error"
-        if 'No results for your query' in data2['Error']:
-            return "Search error"
-        return "Unspecified error"
-    
+    lastSeason = max(map(int, data2['data']['airedSeasons']))
     if data['data']['seriesId'] == "":
         seriesId = '999999'
     else:
         seriesId = data['data']['seriesId']
-
-
-    fields = ['',
+    if data['data']['status'] == 'Ended':
+        #collect info about last episode name
+        URL3 = URL + '/series/' + str(filmID) + '/episodes/query'
+        HEADERS = {'Content-Type': 'application/json','Authorization':('Bearer ' + token),'Accept-Language':'ru'}
+        PARAMS = {'airedSeason':str(lastSeason),}
+        r = requests.get(url = URL3, headers = HEADERS, params = PARAMS)
+        data3 = r.json()
+        maxEpnum = 1
+        for episod in data3['data']:
+            if int(episod['airedEpisodeNumber']) > maxEpnum:
+                maxEpnum = int(episod['airedEpisodeNumber'])
+                maxEp = episod
+        if  maxEp['episodeName'] == None:
+            #no rus name
+            HEADERS = {'Content-Type': 'application/json','Authorization':('Bearer ' + token)}
+            r = requests.get(url = URL3, headers = HEADERS, params = PARAMS)
+            data3 = r.json()
+            maxEpnum = 1
+            for episod in data3['data']:
+                if int(episod['airedEpisodeNumber']) > maxEpnum:
+                    maxEpnum = int(episod['airedEpisodeNumber'])
+                    maxEp = episod
+        firstAired = maxEp['firstAired']
+        airedEpisodeNumber = maxEp['airedEpisodeNumber']
+        episodeName = maxEp['episodeName']
+    else:
+        firstAired = 'TBD'
+        airedEpisodeNumber = 'TBD'
+        episodeName = 'TBD'
+    #
+    f = open('films.csv', mode="r", encoding="utf-8")
+    myid = int((list(f)[-1]).split('\t')[0]) + 1
+    f.close()
+    #summ result in the finnal line
+    fields = [str(myid),
         data['data']['id'],
         data['data']['imdbId'],
         seriesId,
         data['data']['seriesName'],
-        '',
+        dataru['data']['seriesName'],
         data['data']['status'],
         data['data']['firstAired'],
         data['data']['network'],
-        max(map(int, data2['data']['airedSeasons'])),
+        str(lastSeason),
         data2['data']['airedEpisodes'],
-        data['data']['siteRating']
+        data['data']['siteRating'],
+        firstAired,
+        airedEpisodeNumber,
+        episodeName,
         ]
-
     f = open('films.csv', mode="a", encoding="utf-8")
-    csv_writer = csv.writer(f, delimiter='\t')
+    csv_writer = csv.writer(f, lineterminator='\n', delimiter='\t')
     csv_writer.writerow(fields)
     f.close()
+
+#test add series :)
+tvdbGetSerialInfo(281534)
+tvdbGetSerialInfo(70533)
+tvdbGetSerialInfo(323168)
+
+
+
+"""
 
 def getLastEpisodData(filmID, airedSeason): 
     # intId = 8   
@@ -146,7 +208,7 @@ def updateCsv():
 
             writer.writerows(all)
 
-
+"""
 """series_id = open("series_id.txt", 'r')
 for line in series_id.readlines():
     id = str.rstrip(line)
@@ -154,8 +216,4 @@ for line in series_id.readlines():
     print(id + ' completed')
 """
 #getLastEpisodData(75760,9)
-
-
-
-
 #updateCsv()
