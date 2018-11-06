3d3ef57466368580546b13bd5c0846adabbbb7e7
art_yomov <artemov.sergey1989@gmail.com>
2018-11-06 20:07:53 +0800

    5000 port

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 #
+
 from __future__ import unicode_literals
 
 import json
@@ -19,10 +20,96 @@ sessionStorage = {}
 # Start flask with POST method listening
 @app.route("/", methods=['POST'])
 
+
+
 #main function
 def main():
-    logging.info('Request: %r', request.json)
+    # Hanle dialog
+    def handle_dialog(req, res):
+        user_id = req['session']['user_id']
+        if req['session']['new']:
+            #New user, return welcome message
+            variants = ['Привет, что ищешь?',
+                'Приветсвую, какой сериал интересует?',
+                'Приветсвую, я помогаю узнать дату выхода новой серии вашего любимого сериала. Какой интересует?',
+                'Здравствуй, о какой серии ищите информацию?',
+                'Помогаю найти информацию о тате выхода свежей серии. Кстати, зрасьте :)',
+                'Привет. Буду рада подсказать дату выхода новой серии. Какой сериал?',]     
+            res['response']['text'] = random.choice(variants)
+            return
 
+        # Take user text
+        text = req['request']['command'].lower()
+
+        #check for key words
+        keywords = ['добавить сериал', 'подробнее', 'сериал', 'смотреть']
+        if text in keywords:
+            if user_id not in sessionStorage:
+                res['response']['text'] = 'Я потеряла нить нашей беседы :)'
+                return
+            if (text == 'добавить сериал') and (sessionStorage[user_id] == 0):
+                #Добавление сериала и сессия была (не пустой)
+                res['response']['text'] = 'Спасибо! Мы проверим ваше обращение и добавим интересующий сериал в ближайшее вермя'
+                return
+            if text == 'подробнее':
+                if sessionStorage[user_id] != 0:
+                    #res['response']['text'] = getFilmInfoLocal(str(sessionStorage[user_id]))
+                    res['response']['text'] = 'Заглушка для подробной информации о серии'            
+                else:
+                    res['response']['text'] = 'Я потеряла нить нашей беседы :)'
+                return
+            if text == 'сериал':
+                if sessionStorage[user_id] != 0:
+                    res['response']['text'] = getFilmInfoLocal(str(sessionStorage[user_id]))
+                else:
+                    res['response']['text'] = 'Я потеряла нить нашей беседы :)'
+                return
+            if text == 'смотреть':
+                if sessionStorage[user_id] != 0:
+                    res['response']['text'] = 'Слышала, что есть альтернативные вариаты просмотра ;)'
+                    res['response']['end_session'] = 'true'
+                else:
+                    res['response']['text'] = 'Я потеряла нить нашей беседы :)'
+                return
+        #no more key works, execute seach function on top of this text
+        # print('test search')
+        result = CoreSearch(text)
+        #save intId into dictionary
+        sessionStorage[user_id] = result[1]
+        #return result to user
+        res['response']['text'] = result[0]
+        #res['response']['text'] = CoreSearch(text)
+        #add suggessted buttons
+        if result[1] == 0:
+            #write data to the log file
+            f = open('logs.txt', mode="a+", encoding="utf-8")
+            csv_writer = csv.writer(f, lineterminator='\n', delimiter='\t')
+            csv_writer.writerow([user_id, text, req['meta']['client_id'], req['meta']['locale']])
+            f.close()        
+            res['response']['buttons'] = [
+                {
+                    "title": "Добавить сериал",
+                }
+            ]
+        if result[1] != 0:
+            res['response']['buttons'] = [
+                {
+                    "title": "Подробнее",
+                },
+                {
+                    "title": "Сериал",
+                },
+                {
+                    "title": "Смотреть",
+                    "url": OfficialURL(result[1]),
+                    #"hide": True
+                }
+            ]
+    #logging.info('Request: %r', request.json)
+    
+    print('-------------------------------')
+    print(request.json)
+    print('-------------------------------')
     response = {
         "version": request.json['version'],
         "session": request.json['session'],
@@ -30,10 +117,11 @@ def main():
             "end_session": False
         }
     }
-
+    print(response)
+    
     handle_dialog(request.json, response)
 
-    logging.info('Response: %r', response)
+    #logging.info('Response: %r', response)
 
     return json.dumps(
         response,
@@ -41,86 +129,11 @@ def main():
         indent=2
     )
 
-# Hanle dialog
-def handle_dialog(req, res):
-    user_id = req['session']['user_id']
-    if req['session']['new']:
-        #New user, return welcome message
-        variants = ['Привет, что ищешь?',
-            'Приветсвую, какой сериал интересует?',
-            'Приветсвую, я помогаю узнать дату выхода новой серии вашего любимого сериала. Какой интересует?',
-            'Здравствуй, о какой серии ищите информацию?',
-            'Помогаю найти информацию о тате выхода свежей серии. Кстати, зрасьте :)',
-            'Привет. Буду рада подсказать дату выхода новой серии. Какой сериал?',]     
-        res['response']['text'] = random.choice(variants)
-        return
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '0.0.0.0')    
+    
+    
 
-    # Take user text
-    text = req['request']['command'].lower()
-
-    #check for key words
-    keywords = ['добавить сериал', 'подробнее', 'сериал', 'смотреть']
-    if text in keywords:
-        if user_id not in sessionStorage:
-            res['response']['text'] = 'Я потеряла нить нашей беседы :)'
-            return
-        if (text == 'добавить сериал') and (sessionStorage[user_id] == 0):
-            #Добавление сериала и сессия была (не пустой)
-            res['response']['text'] = 'Спасибо! Мы проверим ваше обращение и добавим интересующий сериал в ближайшее вермя'
-            return
-        if text == 'подробнее':
-            if sessionStorage[user_id] != 0:
-                #res['response']['text'] = getFilmInfoLocal(str(sessionStorage[user_id]))
-                res['response']['text'] = 'Заглушка для подробной информации о серии'            
-            else:
-                res['response']['text'] = 'Я потеряла нить нашей беседы :)'
-            return
-        if text == 'сериал':
-            if sessionStorage[user_id] != 0:
-                res['response']['text'] = getFilmInfoLocal(str(sessionStorage[user_id]))
-            else:
-                res['response']['text'] = 'Я потеряла нить нашей беседы :)'
-            return
-        if text == 'смотреть':
-            if sessionStorage[user_id] != 0:
-                res['response']['text'] = 'Слышала, что есть альтернативные вариаты просмотра ;)'
-                res['response']['end_session'] = 'true'
-            else:
-                res['response']['text'] = 'Я потеряла нить нашей беседы :)'
-            return
-    #no more key works, execute seach function on top of this text
-    # print('test search')
-    result = CoreSearch(text)
-    #save intId into dictionary
-    sessionStorage[user_id] = result[1]
-    #return result to user
-    res['response']['text'] = result[0]
-    #res['response']['text'] = CoreSearch(text)
-    #add suggessted buttons
-    if result[1] == 0:
-        #write data to the log file
-        f = open('logs.txt', mode="a+", encoding="utf-8")
-        csv_writer = csv.writer(f, lineterminator='\n', delimiter='\t')
-        csv_writer.writerow([user_id, text, req['meta']['client_id'], req['meta']['locale']])
-        f.close()        
-        res['response']['buttons'] = [
-            {
-                "title": "Добавить сериал",
-            }
-        ]
-    if result[1] != 0:
-        res['response']['buttons'] = [
-            {
-                "title": "Подробнее",
-            },
-            {
-                "title": "Сериал",
-            },
-            {
-                "title": "Смотреть",
-                "url": OfficialURL(result[1]),
-                #"hide": True
-            }
-        ]
 
 
